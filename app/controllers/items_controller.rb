89d06af9680e0194cec0e4761764e55ccd180e59
ooyamadaizan <daizandeisia@gmail.com>
2025-01-23 23:33:43 +0900

    複数の商品名検索
    商品名に半角スペースで区切って複数の商品を検索できるようにOR検索で再設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,14 @@ class ItemsController < ApplicationController
   end
 
   def search
-    @q = Item.ransack(params[:q])
-    @items = @q.result
+    # params[:q]がnilではない且つ、params[:q][:name]がnilではないとき（商品名の欄が入力されているとき）
+    # if params[:q] && params[:q][:name]と同じような意味合い
+    if params[:q]&.dig(:name)
+      # squishメソッドで余分なスペースを削除する
+      squished_keywords = params[:q][:name].squish
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:name_cont_any] = squished_keywords.split(" ")
+    end
   end
 
   private
